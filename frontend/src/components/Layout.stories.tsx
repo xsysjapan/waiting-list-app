c80496381602dd721adf04de5bf23ae8f700be05
Yuto Nagano <chamelion@hotmail.co.jp>
2021-05-14 10:07:28 +0900

    frontend プロジェクトに storybook を導入

---
+++ frontend/src/components/Layout.stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react'
+import { Layout, LayoutProps } from './Layout';
+
+export default {
+    component: Layout,
+    title: 'components/Layout'
+} as Meta;
+
+const Template: Story<LayoutProps> = args => <Layout {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+    children: 'children'
+}
---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export type LayoutProps = React.PropsWithChildren<{}>;
+
+export const Layout = (props: LayoutProps) => {
+    const { children } = props;
+    return <div>{children}</div>
+}
+
+export default Layout;
