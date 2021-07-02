4bf20cfbd27059b1d0040bec59f093f44cf2815a
Yuto Nagano <chamelion@hotmail.co.jp>
2021-07-03 01:49:20 +0900

    書き方を修正

--- backend/src/services/sms.service.ts
+++ backend/src/services/sms.service.ts
@@ -1,9 +1,9 @@
 import AWS from "aws-sdk";
 AWS.config.update({ region: process.env.AWS_DEFAULT_REGION });
-
+const snsClient = new AWS.SNS({ apiVersion: "2010-03-31" });
 export class SmsService {
   public async sendMessage(phoneNumber: string, message: string) {
-    const data = await new AWS.SNS({ apiVersion: "2010-03-31" })
+    const data = await snsClient
       .publish({
         Message: message,
         PhoneNumber: `81${
